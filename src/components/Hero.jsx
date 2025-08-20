252096c3c5a04d49f04b7a6992f11019889b4466
daffanugraha21 <dnr.project12@gmail.com>
2025-08-20 12:28:28 +0700

    Update Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -99,18 +99,9 @@ export default function Hero() {
               />
             </div>
             <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-              {/* <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{" "}
-              <a href="#" className="font-semibold text-indigo-600">
-                <span aria-hidden="true" className="absolute inset-0" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
-          </div> */}
               <div className="text-center">
                 <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-                  Belajar Linkedin
+                  Mastering LinkedIn for Career Growth
                 </h1>
                 <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
                   Belajar Linkedin dari 0 sampai keterima kerja
